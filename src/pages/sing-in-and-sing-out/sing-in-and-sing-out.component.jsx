9083810ff6c86d4297ad5b8324374a4f55ea7a20
Victor Prado <vicknan0182@gmail.com>
2021-10-23 20:48:54 -0500

    implemented firebase utils, included ability to store authenticated users inot our firestore database

--- src/pages/sing-in-and-sing-out/sing-in-and-sing-out.component.jsx
+++ src/pages/sing-in-and-sing-out/sing-in-and-sing-out.component.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 import SingIn from "../../components/sing-in/sing-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
+
+import "./sign-in-and-sign-up.styles.scss";
 
 export default function SingInAndSingOutPage() {
   return (
-    <div className="sing-in-and-sing-out">
+    <div className="sign-in-and-sign-up">
       <SingIn />
+      <SignUp />
     </div>
   );
 }
