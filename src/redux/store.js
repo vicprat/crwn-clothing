32af8c9bd5e851517641d9bb8ddc7a0d6bc65809
Victor Prado <vicknan0182@gmail.com>
2021-12-22 21:28:48 -0600

    moved our directory and shpo data into its own reducer, created respective selectors and updated correspondig coomponents with ux flow for directory/shop

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
 import logger from "redux-logger";
 
 import rootReducer from "./root-reducer";
 
 const midlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...midlewares));
+export const store = createStore(rootReducer, applyMiddleware(...midlewares));
 
-export default store;
+export const persistor = persistStore(store);
+
+
